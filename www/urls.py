ccaf804b2773d7c8475fe89395f8403a7ae16771
SongGithub <songjin@hotmail.com>
2014-04-16 15:11:25 +1000

    require.txt

--- www/urls.py
+++ www/urls.py
@@ -19,11 +19,16 @@ urlpatterns = patterns('',
         name='category-update-delete'
     ), 
 
-    url(r'^categories/(?P<slug>[\w-]+)/items$', 
-        views.CategoryItemListView.as_view(), name='category-item-list'),
+    url(
+        r'^categories/(?P<slug>[\w-]+)/items$', 
+        views.CategoryItemListView.as_view(), name='category-item-list'
+    ),
 
-    url(r'^categories/(?P<cat_slug>[\w-]+)/items/(?P<slug>[\w-]+)$', 
-        views.CategoryItemUpdateDeleteView.as_view(), name='category-item-update-delete'), 
+    url(
+        r'^categories/(?P<cat_slug>[\w-]+)/items/(?P<slug>[\w-]+)$', 
+        views.CategoryItemUpdateDeleteView.as_view(), 
+        name='category-item-update-delete'
+    ), 
 
     url(r'^admin/', include(admin.site.urls)),
 )
